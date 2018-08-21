87535caa0060a8eebcffc15a798acfee9e858009
kasseyrodriguez <kassseyrodriguez@gmail.com>
2018-08-21 01:10:30 -0400

    change name of auth

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-    http_basic_authenticate_with name: "kass", password: "1234", except: [:index, :show]
+    http_basic_authenticate_with name: "kassey", password: "1234", except: [:index, :show]
     def index
         @posts = Post.all
     end
